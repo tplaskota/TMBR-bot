06458ec06024f91b2742439a5b93e195093b1d3a
tplaskota <tplaskota@gmail.com>
2016-06-27 16:39:06 +0200

    Further cleanup, add edit options

--- tmbr.py
+++ tmbr.py
@@ -1,13 +1,13 @@
 import praw
-import pprint
+from pprint import pprint
 from prawoauth2 import PrawOAuth2Mini
 
 from peewee import *
 from peewee import OperationalError
 from peewee import DoesNotExist
 
-from tmbr_tokens import app_key, app_secret, access_token, refresh_token
-from tmbr_settings import scopes, user_agent, bot_name
+from tokens import app_key, app_secret, access_token, refresh_token
+from settings import scopes, user_agent, bot_name
 
 reddit_client = praw.Reddit(user_agent=user_agent)
 oauth_helper = PrawOAuth2Mini(reddit_client,app_key=app_key,app_secret=app_secret,access_token=access_token,scopes=scopes,refresh_token=refresh_token)
@@ -19,18 +19,12 @@ last_checked_comment = []
 
 response_head = "Hi!\n\n"
 response_tail = "\n\n-------------------------------------------------\n\n^^I ^^am ^^a ^^bot. ^^You ^^can ^^complain ^^to ^^my ^^master ^^/u/Terdol ^^or ^^mods ^^at ^^/r/TMBR"
-
-class RepliedComments(Model):
-    comment_id = CharField()
-    author = CharField()
-    subreddit = CharField()
-
-    class Meta:
-        database = db
         
 class CountingSubmission(Model):
     submission_id = CharField()
     bot_comment_id = CharField()
+    author = CharField()
+    subreddit = CharField()
     
     class Meta:
         database = db
@@ -38,29 +32,19 @@ class CountingSubmission(Model):
 def initialize_db():
     db.connect()
     try:
-        db.create_tables([RepliedComments,CountingSubmission])
+        db.create_tables([CountingSubmission,])
     except OperationalError:
         pass
 
 def deinit():
     db.close()
 
-def is_already_replied(comment_id):
-    if comment_id in replied_comments:
-        return True
-    try:
-        RepliedComments.select().where(
-            RepliedComments.comment_id == comment_id).get()
-        return True
-    except DoesNotExist:
-        return False
-
-def log_this_comment(comment, TableName=RepliedComments):
-    comment_data = TableName(comment_id=comment.id,
+def log_this_comment(comment, TableName=CountingSubmission):
+    comment_data = TableName(bot_comment_id=comment.id,
                              author=comment.author.name,
-                             subreddit=comment.subreddit.display_name)
+                             submission_id=comment.parent_id[3:], subreddit=comment.subreddit.display_name)
     comment_data.save()
-    replied_comments.append(comment.id)
+    counting_submissions.append(comment.parent_id[3:])
     
 def already_has_bot_comment(submission_id):
     if submission_id in counting_submissions:
@@ -82,17 +66,18 @@ def counter_table(a,b,c):
     result += ' '*(10-len(str(b)))+str(b)+'|\n'
     result += 'undecided |'
     result += ' '*(10-len(str(b)))+str(c)+'|\n'
+    return result
 
-def make_new_comment(_submission_id,TableName=CountingSubmission):
-    submission_data = TableName(submission_id=_submission_id,
-                                bot_comment_id = '0')
-    submission_data.save()
-    counting_submissions.append(_submission_id)
-    submission = reddit_client.get_submission(submission_id=_submission_id)
-    #pprint.pprint(vars(submission))
-    #pprint.pprint(dir(submission))
-    submission.add_comment()
+def make_new_comment(_submission_id,a=0,b=0,c=0,TableName=CountingSubmission):
+    sub = reddit_client.get_submission(submission_id=_submission_id)
+    response = response_head + counter_table(a,b,c) + response_tail
+    comment = sub.add_comment(response)
+    log_this_comment(comment)
 
+def edit_comment(comment,a=0,b=0,c=0):
+    response = response_head + counter_table(a,b,c) + response_tail
+    comment.edit(response)
+    
 def check_condition(c):
     if "meme" in c.body.lower():
         for rep in c.replies:
@@ -100,36 +85,19 @@ def check_condition(c):
                 return False
         return True
     return False
-
-def bot_action(c):
-    print(c.body)
     
 def clear_subreddit(sub):
     for c in reddit_client.get_comments(sub):
         if c.author.name==bot_name:
             c.delete()
-    q = RepliedComments.delete().where(str(RepliedComments.subreddit).lower() == sub.lower())
+    q = CountingSubmission.delete().where(str(CountingSubmission.subreddit).lower() == sub.lower())
     q.execute()
     
 def remove_downvoted():
     for c in bot_name.get_comments(limit=None):
         if c.score<0:
             c.delete()
-            
-def comment_is_assigned(c):
-    try:
-        s = CountingSubmission.select().where(
-            CountingSubmission.submission_id == c.link_id[3:]).get()
-        return s.bot_comment_id != '0'
-    except DoesNotExist:
-        return False
-        
 
-def try_to_assign_comment(c):
-    pass
-    
-def deal_with_submissions():
-    pass
 
 def main_loop():
     #for c in praw.helpers.comment_stream(reddit_client, 'TMBR'):
