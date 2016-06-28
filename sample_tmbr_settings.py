06165ec2c32e4fd629647387a017e903adc5c342
tplaskota <tplaskota@gmail.com>
2016-06-28 11:58:09 +0200

    adding settings.bot_password

--- sample_tmbr_settings.py
+++ sample_tmbr_settings.py
@@ -3,3 +3,4 @@ import os
 scopes = {'edit', 'flair', 'save', 'read', 'vote', 'history', 'submit', 'modconfig', 'identity','modcontributors', 'modflair', 'modlog', 'modothers', 'modposts', 'modself', 'modwiki', 'mysubreddits', 'privatemessages','report','subscribe','wikiedit','wikiread'}
 user_agent = ("TMBR-BOT v0.1")
 bot_name = '..'
+bot_password = '..'
