08e97fd29bc456a618c4a2e7309ace3cf422b425
tplaskota <tplaskota@gmail.com>
2016-12-19 09:55:42 +0100

    final fixes after debate stuff

--- tmbr.py
+++ tmbr.py
@@ -26,6 +26,7 @@ u'\u2738',u'\u2742',u'\u2739']
 response_head = ""
 response_tail = "\n\n-------------------------------------------------\n\n^^I ^^am ^^a ^^bot. ^^You ^^can ^^complain ^^to ^^my ^^master ^^/u/Terdol ^^or ^^mods ^^at ^^/r/TMBR"
 bot_commands = ["!agreewithop","!disagreewithop","!undecided"]
+debate_rules_link = "https://www.reddit.com/r/TMBR/comments/5i2q75/temporarynew_feature_test_i_believe_rtmbr_is/"
         
 class CountingSubmission(Model):
     submission_id = CharField()
@@ -95,30 +96,47 @@ def debate_rules(tag='Debate'):
     result += 'Quick reminder of posting rules in debate threads:\n\n'
     result += '* Redditors with flair might comment freely, but are unable to add their votes to automatic poll.\n'
     result += '* Redditors without flair can only add their votes to automatic poll via usuall commands, but can not comment anything else.\n\n'
-    result += 'In case of breaking these restrictions comments will be removed without warning. For more information visit (here)['+debate_rules_link+'].\n'
+    result += 'In case of breaking these restrictions comments will be removed without warning. For more information visit [here]('+debate_rules_link+').\n'
+    return result
+
+def can_claim_flair_text(user_name = None):
+    result = '\n\n'
+    result += "Congratulations"
+    if user_name:
+         result += " /u/"+user_name
+    result += " on reaching 50 comment mark!\n\n"
+    result += " You can contact modmail for your flair!\n\n"
     return result
 
 def make_new_comment(_submission_id,a=0,b=0,c=0,TableName=CountingSubmission):
     global reddit_client
     sub = reddit_client.get_submission(submission_id=_submission_id)
-    print(_submission_id)
+    print('attempting to create new comment in submission '+_submission_id)
     try:
         response = response_head + counter_table(a,b,c)
         if sub.link_flair_text != None and 'debate' in sub.link_flair_text.lower():
             response += debate_rules(sub.link_flair_text)
         response += response_tail
+        print('comment prepared')
         comment = sub.add_comment(response)
+        print('comment successfully posted')
         #sticky - requires login on mod
         comment.distinguish(sticky=True)
+        print('comment successfully sticked')
         log_this_comment(comment)
     except praw.errors.APIException as e:
+        print('something went wrong in creating comment')
         return False
     return True
 
-def edit_comment(comment,b_debate=False,a=0,b=0,c=0):
+def edit_comment(comment,a=0,b=0,c=0,b_debate=False, can_claim_flair=None):
+    print("editing comment in submission "+comment.link_id[3:]+' with votes: '+str(a)+', '+str(b)+', '+str(c))
     response = response_head + counter_table(a,b,c)
     if b_debate:
-        response += debate_rules(sub.link_flair_text)
+        response += debate_rules()
+    elif can_claim_flair:
+        print("user /u/"+can_claim_flair+" has reached 50 comments for the first time, adding information to poll")
+        response += can_claim_flair_text(can_claim_flair)
     response += response_tail
     comment.edit(response)
     
@@ -141,15 +159,19 @@ def remove_downvoted():
 def recalculate_active_submissions():
     global reddit_client
     global active_submissions
+    print('Current active submissions:' + ', '.join(active_submissions))
     for id in active_submissions:
         votes=[[],[],[],]
         banned_on_this_submission = []
         bot_comment = None
         one_bot_comment_flag = False
         b_debate_submission = False
+        can_claim_flair = None
         while not one_bot_comment_flag:
             sub = reddit_client.get_submission(submission_id=id)
             b_debate_submission = sub.link_flair_text != None and 'debate' in sub.link_flair_text.lower()
+            if sub.num_comments > 50 and sub.author != None and sub.author.name not in moderator_list and sub.banned_by == None and tmbr_subreddit.get_flair(sub.author)['flair_text'] == None:
+                can_claim_flair = sub.author.name
             sub.replace_more_comments(limit=None,threshold=0)
             flat_comments = praw.helpers.flatten_tree(sub.comments)
             bot_comment = [com for com in flat_comments if com.author != None and com.author.name.lower() == bot_name.lower()]
@@ -159,10 +181,15 @@ def recalculate_active_submissions():
                 for com in bot_comment:
                     if not com.stickied:
                         com.delete()
+                        print('removed unsticked bot comment from submission '+id)
             elif len(bot_comment) == 1:
                 one_bot_comment_flag = True
+                print('submission '+id+' has one bot comment')
             elif len(bot_comment) == 0:
                 make_new_comment(id)
+                print('submission '+id+' has no bot comment, creating new comment')
+                print('waiting 60 seconds to register comment in api')
+                time.sleep(60) #takes time for api to register
         
             
         for com in flat_comments:
@@ -175,6 +202,8 @@ def recalculate_active_submissions():
                 else:
                     #com.delete()
                     continue
+            if com.banned_by != None:
+                continue
             if com.author.name in banned_on_this_submission:
                 continue
             command_index = None
@@ -219,13 +248,13 @@ def recalculate_active_submissions():
                 time.sleep(3)
                 if type(bot_comment) is list:
                     bot_comment=bot_comment[0]
-                edit_comment(bot_comment, b_debate_submission, *[len(a) for a in votes])
+                edit_comment(bot_comment, *[len(a) for a in votes],b_debate_submission, can_claim_flair)
     active_submissions = []
                 
 def scan_comments_for_activity():
     global reddit_client
     global active_submissions
-    for c in reddit_client.get_comments('TMBR'):
+    for c in reddit_client.get_comments('TMBR', limit=100):
         if 1 != len([1 for command in bot_commands if command in c.body.lower()]): #check agree/disagre/undecided, only one of those can be present
             continue
         if c.author == None: #comment deleted
@@ -266,10 +295,14 @@ def moderate_debates():
             if tmbr_subreddit.get_flair(com.author)['flair_text'] != None:
                 #flaired user
                 if 0 < len([1 for command in bot_commands if command in com.body.lower()]):
+                    if com.banned_by != None:
+                        continue
                     com.remove()
             else:
                 #not-flaired user
                 if 1 != len([1 for command in bot_commands if command == com.body.lower()]):
+                    if com.banned_by != None:
+                        continue
                     com.remove()
         
         
